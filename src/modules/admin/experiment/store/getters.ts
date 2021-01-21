77796b94c26e52e72104e7eb709ac39d565f02ff
lacau <lacau@zpay.com.br>
2021-01-21 11:42:40 -0300

    Scale fimH range

--- src/modules/admin/experiment/store/getters.ts
+++ src/modules/admin/experiment/store/getters.ts
@@ -83,7 +83,7 @@ const getters: GetterTree<ExperimentState, unknown> = {
     return createOptions(1500, 'wzc_')
   },
   fimType() {
-    return createOptions(200, 'fimH')
+    return createOptions(500, 'fimH')
   },
 }
 
