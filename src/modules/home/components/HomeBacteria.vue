310b64143af485efd63074e3e85c3b730aa7214a
Lacau <leandro.lacau@epag.com>
2025-05-12 18:40:17 -0300

    Change home icons to always show

--- src/modules/home/components/HomeBacteria.vue
+++ src/modules/home/components/HomeBacteria.vue
@@ -1,46 +1,35 @@
 <template>
   <v-container class="home-bacteria">
-    <vue-glide :options="options" :bullet="$vuetify.breakpoint.smAndDown">
-      <vue-glide-slide
+    <v-row class="mb-10" justify="center">
+      <figure class="text-center pa-3"
         v-for="bacterium in bacteria"
         :key="bacterium.name"
       >
-        <figure class="text-center pa-3">
-          <img
-            :alt="`Bacterium ${bacterium.name}`"
-            :src="bacterium.image"
-            width="161"
-            height="120"
-          >
-          <figcaption>
-            <h1 class="display-1 font-weight-medium mb-2 mt-3">
-              {{ $t(`home.bacteria.${bacterium.name}`) }}
-            </h1>
-            <p class="title font-italic font-weight-regular mb-1">
-              {{ bacterium.scientificName }}
-            </p>
-            <p class="body-2 grey--text text--darken-1 mb-8">
-              {{ bacterium.origin }}
-            </p>
-            <primary-button :to="{
-              name: 'bacteria',
-              params: { name: bacterium.name }
-            }">
-              {{ $t('home.see_map') }}
-            </primary-button>
-          </figcaption>
-        </figure>
-      </vue-glide-slide>
-
-      <template slot="control">
-        <button data-glide-dir="<">
-          <Icon name="chevron_left" />
-        </button>
-        <button data-glide-dir=">">
-          <Icon name="chevron_right" />
-        </button>
-      </template>
-    </vue-glide>
+        <img
+          :alt="`Bacterium ${bacterium.name}`"
+          :src="bacterium.image"
+          width="161"
+          height="120"
+        >
+        <figcaption>
+          <h1 class="display-1 font-weight-medium mb-2 mt-3">
+            {{ $t(`home.bacteria.${bacterium.name}`) }}
+          </h1>
+          <p class="title font-italic font-weight-regular mb-1">
+            {{ bacterium.scientificName }}
+          </p>
+          <p class="body-2 grey--text text--darken-1 mb-8">
+            {{ bacterium.origin }}
+          </p>
+          <primary-button :to="{
+            name: 'bacteria',
+            params: { name: bacterium.name }
+          }">
+            {{ $t('home.see_map') }}
+          </primary-button>
+        </figcaption>
+      </figure>
+    </v-row>
   </v-container>
 </template>
 
@@ -96,7 +85,7 @@ export default class HomeBacteria extends Vue {
   padding: 51px 0 0;
 
   @media #{map-get($display-breakpoints, 'lg-and-up')} {
-    padding: 84px 0 0;
+    padding: 30px 0 0;
   }
 }
 </style>
